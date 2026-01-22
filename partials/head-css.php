9e5056f68755eeab3d21c0d41b15903aaf790e22
desarrollovolcan <eisla@fvolcan.cl>
2026-01-22 18:35:03 -0300

    Add login logo size setting

--- partials/head-css.php
+++ partials/head-css.php
@@ -3,6 +3,11 @@ require_once __DIR__ . '/../app/bootstrap.php';
 $municipalidad = get_municipalidad();
 $primaryColor = $municipalidad['color_primary'] ?? '#6658dd';
 $secondaryColor = $municipalidad['color_secondary'] ?? '#4a81d4';
+$logoPath = $municipalidad['logo_path'] ?? 'assets/images/logo.png';
+$logoTopbarHeight = $municipalidad['logo_topbar_height'] ?? 56;
+$logoLoginHeight = $municipalidad['logo_login_height'] ?? 48;
+$logoSidenavHeight = $municipalidad['logo_sidenav_height'] ?? 48;
+$logoSidenavHeightSm = $municipalidad['logo_sidenav_height_sm'] ?? 36;
 $primaryRgb = hex_to_rgb($primaryColor) ?? [102, 88, 221];
 $secondaryRgb = hex_to_rgb($secondaryColor) ?? [74, 129, 212];
 ?>
@@ -16,6 +21,9 @@ $secondaryRgb = hex_to_rgb($secondaryColor) ?? [74, 129, 212];
 <!-- App css -->
 <link href="assets/css/app.min.css" rel="stylesheet" type="text/css">
 
+<!-- Favicon -->
+<link rel="icon" href="<?php echo htmlspecialchars($logoPath, ENT_QUOTES, 'UTF-8'); ?>" type="image/png">
+
 <style>
     :root {
         --ins-primary: <?php echo htmlspecialchars($primaryColor, ENT_QUOTES, 'UTF-8'); ?>;
@@ -26,6 +34,10 @@ $secondaryRgb = hex_to_rgb($secondaryColor) ?? [74, 129, 212];
         --bs-primary-rgb: <?php echo (int) $primaryRgb[0]; ?>, <?php echo (int) $primaryRgb[1]; ?>, <?php echo (int) $primaryRgb[2]; ?>;
         --bs-secondary: <?php echo htmlspecialchars($secondaryColor, ENT_QUOTES, 'UTF-8'); ?>;
         --bs-secondary-rgb: <?php echo (int) $secondaryRgb[0]; ?>, <?php echo (int) $secondaryRgb[1]; ?>, <?php echo (int) $secondaryRgb[2]; ?>;
+        --ins-topbar-logo-height: <?php echo (int) $logoTopbarHeight; ?>px;
+        --ins-login-logo-height: <?php echo (int) $logoLoginHeight; ?>px;
+        --ins-logo-lg-height: <?php echo (int) $logoSidenavHeight; ?>px;
+        --ins-logo-sm-height: <?php echo (int) $logoSidenavHeightSm; ?>px;
     }
 
     .side-nav-title {
--- partials/topbar.php
+++ partials/topbar.php
@@ -10,17 +10,17 @@
                         <img src="assets/images/logo.png" alt="logo">
                     </span>
                     <span class="logo-sm">
-                        <img src="assets/images/logo-sm.png" alt="small logo">
+                        <img src="assets/images/logo.png" alt="logo">
                     </span>
                 </a>
 
                 <!-- Logo Dark -->
                 <a href="index.php" class="logo-dark">
                     <span class="logo-lg">
-                        <img src="assets/images/logo-black.png" alt="dark logo">
+                        <img src="assets/images/logo.png" alt="logo">
                     </span>
                     <span class="logo-sm">
-                        <img src="assets/images/logo-sm.png" alt="small logo">
+                        <img src="assets/images/logo.png" alt="logo">
                     </span>
                 </a>
             </div>
